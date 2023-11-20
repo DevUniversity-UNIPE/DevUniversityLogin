02b74733621ed93eeacc86d3e64527586a19f495
DeyvidXYZ <deyvidlucas_026@outlook.com>
2023-11-20 14:41:05 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="LeftCont">
+            <img src="./image/logoU1.png" class="Logo">
+        </div>
+        <div class="RightCont">
+            <div class="FormCont">
+                <div class="LogoCont">Dev University</div>
+                <form action="" method="post" class="FormContent">
+                    <input type="email" name="Email" id="Email" placeholder="Email" class="inputype">
+                    <input type="password" name="Password" id="Password" placeholder="Password" class="inputype">
+                    <button type="submit" class="SubmitButton">Log in</button>
+                </form>
+                <div class="or">
+                    <p>--------------------- or ---------------------</p>
+                </div>
+                <div class="BottomCont">
+                <div class="Cadastro">
+                    <a href="#" class="Cad">Cadastre-se</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

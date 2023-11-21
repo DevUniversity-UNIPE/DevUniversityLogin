b6f38b69a9132cdb2bf7288ff3248d653ad5fdfa
DeyvidXYZ <deyvidlucas_026@outlook.com>
2023-11-21 15:17:39 -0300

    CADASTRO

---
+++ cadastrar.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+include("");
+
+$nome = mysqli_real_escape_string();
+$matricula = mysqli_real_escape_string();
+$nota1 = mysqli_real_escape_string();
+$nota2 = mysqli_real_escape_string();
+?>

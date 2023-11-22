2a9d97c1fad26426293ef1fa98e80ddbadefe34a
DeyvidXYZ <deyvidlucas_026@outlook.com>
2023-11-22 12:04:04 -0300

    Socorro Deus

--- cadastrar.php
+++ cadastrar.php
@@ -1,3 +1,61 @@
-<?php 
+<?php
+include('conexao.php');
 
+if (isset($_POST['email']) && isset($_POST['senha'])) {
+    $email = $_POST['email'];
+    $email = mysqli_real_escape_string($conexao, $email);
+
+    $senha = $_POST['senha'];
+    $senha = mysqli_real_escape_string($conexao, $senha);
+
+    // Verifica se o e-mail já está cadastrado
+    $sql_check_email = "SELECT email FROM register.emailsenha WHERE email = ?";
+    $stmt_check_email = mysqli_prepare($conexao, $sql_check_email);
+
+    if ($stmt_check_email) {
+        mysqli_stmt_bind_param($stmt_check_email, "s", $email);
+        mysqli_stmt_execute($stmt_check_email);
+        mysqli_stmt_store_result($stmt_check_email);
+
+        if (mysqli_stmt_num_rows($stmt_check_email) > 0) {
+            // E-mail já cadastrado, exiba uma mensagem de erro
+            echo "E-MAIL JÁ CADASTRADO!";
+            mysqli_stmt_close($stmt_check_email);
+            mysqli_close($conexao);
+            exit();
+        }
+
+        mysqli_stmt_close($stmt_check_email);
+    } else {
+        // Erro na preparação da consulta, exiba uma mensagem de erro
+        die("Erro na verificação de e-mail: " . mysqli_error($conexao));
+    }
+
+    // Insere o novo usuário no banco de dados sem criptografar a senha
+    $sql_insert_user = "INSERT INTO register.emailsenha (email, senha) VALUES (?, ?)";
+    $stmt_insert_user = mysqli_prepare($conexao, $sql_insert_user);
+
+    if ($stmt_insert_user) {
+        mysqli_stmt_bind_param($stmt_insert_user, "ss", $email, $senha);
+        mysqli_stmt_execute($stmt_insert_user);
+
+        if (mysqli_stmt_affected_rows($stmt_insert_user) > 0) {
+            // Cadastro realizado com sucesso
+            echo "CADASTRADO COM SUCESSO";
+        } else {
+            // Erro ao cadastrar usuário, exiba uma mensagem de erro
+            echo "Erro ao cadastrar usuário: " . mysqli_error($conexao);
+        }
+
+        mysqli_stmt_close($stmt_insert_user);
+    } else {
+        // Erro na preparação da consulta, exiba uma mensagem de erro
+        die("Erro na preparação da consulta: " . mysqli_error($conexao));
+    }
+
+    mysqli_close($conexao);
+} else {
+    // Dados incompletos, exiba uma mensagem de erro
+    echo "Dados incompletos. Por favor, forneça um e-mail e uma senha.";
+}
 ?>
--- login.php
+++ login.php
@@ -1,39 +1,50 @@
 <?php
 include('conexao.php');
+session_start();
 
-if (empty($_POST['email']) || empty($_POST['senha'])){
-    header('Location: index.php');
-    exit();
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+if ($conexao === false) {
+    die("Erro de conexão com o banco de dados: " . mysqli_connect_error());
 }
 
-$email = mysqli_real_escape_string($conexao, $_POST['email']);
-$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-
-mysqli_select_db($conexao, 'register');
-
-$query = "SELECT email, senha FROM users WHERE email = ? AND senha = ?";
+$query = "SELECT email, senha FROM register.emailsenha WHERE email = ?";
 $stmt = mysqli_prepare($conexao, $query);
 
 if ($stmt) {
-    mysqli_stmt_bind_param($stmt, "ss", $email, $senha);
+    mysqli_stmt_bind_param($stmt, "s", $email);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_store_result($stmt);
 
-    $rows = mysqli_stmt_num_rows($stmt);
+    if (mysqli_stmt_num_rows($stmt) == 1) {
+        mysqli_stmt_bind_result($stmt, $db_email, $db_senha_hash);
+        mysqli_stmt_fetch($stmt);
 
-    //fazer redirecionamento para o painel de notas
-    
-    if ($rows == 1) {
-        $_SESSION['email'] = $email;
-        header('Location: TabelaNotas.php');
+        if (password_verify($senha, $db_senha_hash)) {
+            // Senha correta, proceda com o login
+            $_SESSION['email'] = $email;
+            mysqli_stmt_close($stmt);
+            mysqli_close($conexao);
+            header('Location: TabelaNotas.php');
+            exit();
+        } else {
+            // Senha incorreta, exiba uma mensagem de erro
+            echo "Senha incorreta";
+        }
     } else {
-        header('Location: index.php');
+        // E-mail não encontrado, exiba uma mensagem de erro
+        echo "E-mail não encontrado";
     }
 
     mysqli_stmt_close($stmt);
 } else {
-    echo "Erro na preparação da consulta";
+    // Erro na preparação da consulta, exiba uma mensagem de erro
+    die("Erro na preparação da consulta");
 }
 
 mysqli_close($conexao);
+
+// Se chegou aqui, algo deu errado, redirecione para a página de login
+exit();
 ?>

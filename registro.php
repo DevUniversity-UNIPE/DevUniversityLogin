8b8ab2bf4f9d10043007ba6648068be4a28631ca
Carolina Veloso <annacarolinacampos@hotmail.com>
2023-11-22 13:12:18 -0300

    css registro

--- registro.php
+++ registro.php
@@ -4,28 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rrgistro</title>
+    <link rel="stylesheet" href="registro.css">
+    <title>Registro</title>
+    
 </head>
 
 <body>
+    <div class="logoImg">
+        <div class="LeftCont">
+            <img src="./image/logoU1.png" class="Logo">
+        </div>
+    </div>
     <div class="container">
         <div class="FormCont">
-            <p class="logo">Dev University</p>
+            <p class="welcomeMessage"> Welcome to </p>
+            <p class="logoName">Dev University</p>
             <form action="cadastrar.php" method="post">
-                <label for="Nome">Digite seu nome:</label>
-                <input type="text" placeholder="Nome:" name="Nome">
-
-                <label for="Senha">Digite sua matricula:</label>
-                <input type="text" placeholder="Matricula:" name="Senha">
-
-                <label for="PrimeiraNota">Digite a primeira nota do aluno:</label>
-                <input type="text" placeholder="1ª Nota" name="PrimeiraNota">
-
-
-                <label for="SegundaNota">Digite a segunda nota do aluno(a):</label>
-                <input type="text" placeholder="2ª Nota:" name="SegundaNota">
-
-                <button type="submit">Cadastrar!</button>
+                <div class = "inputPlaceholder">
+                    <input type="text" placeholder="Nome do Aluno:" name="Nome">
+                    <input type="text" placeholder="Matricula:" name="Senha">
+                    <input type="text" placeholder="1ª Nota" name="PrimeiraNota">
+                    <input type="text" placeholder="2ª Nota:" name="SegundaNota">
+                <div>
+                <button type="submit" class="SubmitButton">Cadastrar!</button><br>
             </form>
         </div>
     </div>
